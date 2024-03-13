b8408a05b9bf085ade449d019fc2de67fcb6cb30
Jeongsihwa <ghsmsl6@gmail.com>
2024-03-13 11:02:14 +0900

    simple-vue

---
+++ simple-vue/src/test/java/com/example/simplevue/SimpleVueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplevue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleVueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
